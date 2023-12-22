a2e7331c7a125c60ff2960e87423eecced3f5d7e
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-12-22 18:28:53 +0530

    03_review_carousel comments added to the js file

--- 03_review_carousel/script.js
+++ 03_review_carousel/script.js
@@ -1,65 +1,70 @@
+// object array to store value of user reviews
 const users=[
-    {
+    { // user 1
         url:"user1.jpeg",
         name:"Priyanshi Agrawal",
         title:"Web Developer",
         review:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit."
     },
-    {
+    { // user 2
         url:"user2.jpg",
         name:"Atharv Vani",
         title:"Frontend Developer",
-        review:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit."
+        review:"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
     },
-    {
+    { // user 3
         url:"user3.jpg",
         name:"Arya Verma",
         title:"Operations Lead",
-        review:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit."
+        review:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
     },
-    {
+    { // user 4
         url:"user4.jpeg",
         name:"Pranay Nagpure",
         title:"AI/ML Developer",
-        review:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam sunt in dolores repellendus aut natus accusantium. Autem accusamus nemo reprehenderit."
+        review:"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."
     },
 ]
 
+// getting all elements from document
 const next = document.querySelector("#next");
 const prev = document.querySelector("#prev");
 const img=document.querySelector("#user-img");
 const username=document.querySelector("#name");
 const title=document.querySelector("#title");
 const review=document.querySelector("#review");
-const len= users.length;
-let counter=0;
+const len= users.length; // length of user object array
+let counter=0; // counter keeps track of current position
 
+// handles click for 'next' button
 next.addEventListener("click" , ()=>{
     counter++;
     if(counter<len)
     {
         putValues(counter);
     }
-    else if(counter==len)
+    else if(counter==len) // acts as a infinite loop for array index. ex- 0,1,2,3,0,1,2,3,0,1..........
     {
         counter=0;
         putValues(counter);
     }
 })
 
+// handles click for 'previous' button
 prev.addEventListener("click",()=>{
     counter--;
     if(counter<len && counter>=0)
     {
         putValues(counter);
     }
-    else if(counter<0)
+    else if(counter<0) // acts as a reverse infinite loop for array index. ex- 3,2,1,0,,3,2,1,0,3,2,......
     {
         counter=len-1;
         putValues(counter);
     }
 })
 
+// handles the value to be displyed at current counter index.
 function putValues(index){
     username.textContent= users[index].name;
     title.textContent= users[index].title;
