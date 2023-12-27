c6128cd2952fe8ef60295392f967b6fa71155df4
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-12-28 00:44:18 +0530

    05_slider completed

--- 05_sidebar/script.js
+++ 05_sidebar/script.js
@@ -1,3 +1,17 @@
 // Author : Priyanshi Agrawal
 // Date : 26-12-2023
 // Title : Responsive Sidebar
+
+const menu= document.querySelector("#menu");
+const sidebar= document.querySelector("#sidebar");
+const close= document.querySelector("#close");
+
+menu.addEventListener("click",()=>{
+    sidebar.classList.remove("close");
+    sidebar.classList.add("active");
+})
+
+close.addEventListener("click", ()=>{
+    sidebar.classList.remove("active");
+    sidebar.classList.add("close");
+})
