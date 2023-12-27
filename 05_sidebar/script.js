391d8b7b8d9f4a54bdc3bd9ffe0791b71b18f47c
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-12-28 00:51:48 +0530

    05_slider comments added

--- 05_sidebar/script.js
+++ 05_sidebar/script.js
@@ -2,15 +2,18 @@
 // Date : 26-12-2023
 // Title : Responsive Sidebar
 
+// selecting buttons and elements from the document
 const menu= document.querySelector("#menu");
 const sidebar= document.querySelector("#sidebar");
 const close= document.querySelector("#close");
 
+// handle the click event for opening the sidebar
 menu.addEventListener("click",()=>{
     sidebar.classList.remove("close");
     sidebar.classList.add("active");
 })
 
+// handle the click event for closing the sidebar
 close.addEventListener("click", ()=>{
     sidebar.classList.remove("active");
     sidebar.classList.add("close");
