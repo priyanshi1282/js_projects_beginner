0bc8987089b89446290e64384ba538d32c234cef
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2023-12-28 14:15:46 +0530

    06_modal functionality implemented

--- 06_modal/script.js
+++ 06_modal/script.js
@@ -1,3 +1,15 @@
 // Author : Priyanshi Agrawal
 // Date : 27-12-2023
 // Title : Modal
+
+const openModal= document.querySelector("#open");
+const closeModal =  document.querySelector("#close");
+const modal= document.querySelector(".modal");
+
+openModal.addEventListener("click", ()=>{
+    modal.classList.add("active");
+})
+
+closeModal.addEventListener("click",()=>{
+    modal.classList.remove("active");
+})
