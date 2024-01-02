98f9c1523de4c32bd73ee89950b2cf53bafe5c71
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-02 22:25:33 +0530

    08_menu food array created

--- 08_menu/script.js
+++ 08_menu/script.js
@@ -1,3 +1,118 @@
 // Author : Priyanshi Agrawal
 // Date : 31-12-2023
 // Title : Menu page - filter items
+
+const menu = [
+    {
+        id: 1,
+        item: "avocado toast",
+        price:"$15.99",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"avocado-toast.jpg",
+        tag:"breakfast"
+    },
+    {
+        id: 2,
+        item: "butter pancake",
+        price:"$5.00",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"butter-pancake.jpg",
+        tag:"breakfast"
+    },
+    {
+        id: 3,
+        item: "cold pasta salad",
+        price:"$19.03",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"pasta-salad.jpg",
+        tag:"breakfast"
+    },
+    {
+        id: 4,
+        item: "masala omlette",
+        price:"$8.83",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"masala-omlette.jpg",
+        tag:"lunch"
+    },
+    {
+        id: 5,
+        item: "tofu burger",
+        price:"$10.99",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"tofu-burger.jpg",
+        tag:"lunch"
+    },
+    {
+        id: 6,
+        item: "salmon rice bowl",
+        price:"$16.00",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"salmon-rice-bowl.jpg",
+        tag:"lunch"
+    },
+    {
+        id: 7,
+        item: "baked pasta",
+        price:"$24.50",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"baked-pasta.jpg",
+        tag:"dinner"
+    },
+    {
+        id: 8,
+        item: "noodles with chilli oil eggs",
+        price:"$22.55",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"Noddles-wth-chilli-oil-eggs.jpg",
+        tag:"dinner"
+    },
+    {
+        id: 9,
+        item: "classic lasagna",
+        price:"$23.55",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"classic-lasagna.jpg",
+        tag:"dinner"
+    },
+    {
+        id: 10,
+        item: "paneer tikka",
+        price:"$18.00",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"paneer-tikka.jpg",
+        tag:"dinner"
+    },
+    {
+        id: 11,
+        item: "blueberry shake",
+        price:"$9.00",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"blueberry-shake.jpg",
+        tag:"shakes"
+    },
+    {
+        id: 12,
+        item: "chocolate shake",
+        price:"$9.00",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"chocolate-shake.jpg",
+        tag:"shakes"
+    },
+    {
+        id: 13,
+        item: "oreo shake",
+        price:"$9.00",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"oreo-shake.jpg",
+        tag:"shakes"
+    },
+    {
+        id: 14,
+        item: "strawberry shake",
+        price:"$9.00",
+        info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus velit, possimus voluptates atque consequatur reiciendis!",
+        img:"strawberry-shake.jpg",
+        tag:"shakes"
+    },
+]
