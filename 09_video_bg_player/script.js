571555f56d070d1ada34360f9f8481a1a4387c4d
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-05 22:57:44 +0530

    09_video_bg_player functionality working

--- 09_video_bg_player/script.js
+++ 09_video_bg_player/script.js
@@ -1,3 +1,22 @@
 // Author : Priyanshi Agrawal
 // Date : 04-01-2024
 // Title : background video player
+
+const video = document.querySelector("#video");
+const controls = document.querySelectorAll(".controls button");
+const cover= document.querySelector("#cover");
+video.play();
+controls.forEach(button => {
+    button.addEventListener("click",()=>{
+        if (button.id ==='play') {
+            video.play();
+            cover.classList.remove("active");
+            cover.classList.add("cover")
+        }
+        else{
+            video.pause();
+            cover.classList.remove("cover");
+            cover.classList.add("active");
+        }
+    })
+})
