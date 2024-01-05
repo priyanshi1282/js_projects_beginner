85fcf5edc50e28ad49defa03dd2877747bce10a9
Priyanshi Agrawal <priyanshiagrawal1282@gmail.com>
2024-01-05 23:59:09 +0530

    09_video_bg_player comments added

--- 09_video_bg_player/script.js
+++ 09_video_bg_player/script.js
@@ -2,18 +2,35 @@
 // Date : 04-01-2024
 // Title : background video player
 
-const video = document.querySelector("#video");
-const controls = document.querySelectorAll(".controls button");
-const cover= document.querySelector("#cover");
+// selecting elements from document
+const video = document.querySelector("#video"); // video 
+const controls = document.querySelectorAll(".controls button"); // array of buttons
+const cover= document.querySelector("#cover"); // cover
+
+// initually play the video
+// when website is loaded this runs
 video.play();
+
+// iterating over array of controls button
+// handling click event on button
+// if button has id == play then play the video and add cover class to cover element
+// else pause btn is clicked and add active class to cover
 controls.forEach(button => {
     button.addEventListener("click",()=>{
+
+        // play button clicked
         if (button.id ==='play') {
+
+            // play the video
             video.play();
             cover.classList.remove("active");
             cover.classList.add("cover")
         }
+
+        // pause button clicked
         else{
+
+            // pause the video
             video.pause();
             cover.classList.remove("cover");
             cover.classList.add("active");
